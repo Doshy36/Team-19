dfedbc82f155371aeca27a096e2097ef8535484f
Eigminas Slavinskas <e.slavinskas@newcastle.ac.uk>
2020-03-09 13:34:44 +0000

    Added api route for user reviews and also updated index.js, package.json files

---
+++ Backend/routes/ratings.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var index = require('../index');
+var router = express.Router();
+
+// GET all ratings for a specific place
+router.get('/ratings/:placeId', function(req, res, next){
+
+    var sql = "SELECT * FROM UserRating WHERE placeId=?";
+    var par = req.params.placeId;
+
+    index.db.query(sql, par, (err, result, fields) => {
+        if(err){
+            res.send({"success": false, "message": err.message});
+            throw err;
+        }
+        res.send({"success": true, "message": result});
+    });
+});
+
+// POST rating entered by user for a specific place
+router.post('/ratings', function(req, res, next){
+
+    var sql = "INSERT INTO UserRating (userId, placeId, rating) VALUES (?, ?, ?)";
+    var par = [req.body.userId, req.body.placeId, req.body.rating];
+
+    index.db.query(sql, par, (err, result, fields) => {
+        if(err){
+            res.send({"success": false, "message": err.message});
+            throw err;
+        }
+        res.send({"success": true, "message": result});
+    });
+});
+
+module.exports = router;
