5da8b318b0b4f1357dcc9eb6fa67c7bbfc550c36
Eigminas Slavinskas <e.slavinskas@newcastle.ac.uk>
2020-03-09 14:06:20 +0000

    Added api route for user bookmark toggling

---
+++ Backend/routes/bookmarks.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var index = require('../index');
+var router = express.Router();
+
+// GET all bookmarks for a particular user
+router.get('/bookmarks/:userId', function(req, res, next){
+
+    var sql = "SELECT * FROM UserBookmark WHERE userId=?";
+    var par = req.params.userId;
+
+    index.db.query(sql, par, (err, result, fields) => {
+        if(err){
+            res.send({"success": false, "message": err.message});
+            throw err;
+        }
+        res.send({"success": true, "message": result});
+    });
+});
+
+// POST bookmark specified by the user
+router.post('/bookmarks', function(req, res, next){
+
+    var sql = "INSERT INTO UserBookmark (userId, placeId) VALUES (?, ?)";
+    var par = [req.body.userId, req.body.placeId];
+
+    index.db.query(sql, par, (err, result, fields) => {
+        if(err){
+            res.send({"success": false, "message": err.message});
+            throw err;
+        }
+        res.send({"success": true, "message": result});
+    });
+});
+
+module.exports = router;
