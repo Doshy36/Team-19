8cf3cde495a9416a713dc379f88c35ca0b6d38a3
chloe-661 <chloesunter@gmail.com>
2020-04-14 03:52:56 +0100

    Smooth scrolling & stuff
    +built a little on the slide show
    + added in smooth scrolling
    + made slideshow bigger
    + contact section complete

--- Website 2.0/Scripts/main.js
+++ Website 2.0/Scripts/main.js
@@ -3,7 +3,7 @@ var slideshow = document.getElementById("slideshow");
 const nextImageDelay = 3000;
 let currentImageCounter = 0;
 
-var images = ["url('./Assets/Alnwick_Castle.jpg')", "url('./Assets/HadriansWall.jpg')"]
+var images = ["url('./Assets/Alnwick_Castle.jpg')", "url('./Assets/HadriansWall.jpg')"];
 
 
 setInterval(nextImage, nextImageDelay);
@@ -12,3 +12,35 @@ function nextImage() {
   currentImageCounter = (currentImageCounter+1) % images.length;
   slideshow.style.backgroundImage = images[currentImageCounter];
 }
+
+
+
+/* --- SLIDESHOW DOWN ARROW CHANGING --- */
+
+var down_arrow = document.getElementById("slideshow-down-arrow-img");
+
+down_arrow.addEventListener('mouseover', function(){
+  down_arrow.src = "Assets/arrow_down_blue.png";
+})
+
+down_arrow.addEventListener('mouseout', function(){
+    down_arrow.src = "Assets/arrow_down.png";
+})
+
+/* --- SMOOTH SCROLLING --- */
+
+var anchorSelector = 'a[href^="#"]'; 
+      
+var anchorList = document.querySelectorAll(anchorSelector); 
+
+anchorList.forEach(link => { 
+    link.onclick = function (e) { 
+        e.preventDefault(); 
+   
+        var destination = document.querySelector(this.hash); 
+  
+        destination.scrollIntoView({ 
+            behavior: 'smooth' 
+        }); 
+    }
+}); 
